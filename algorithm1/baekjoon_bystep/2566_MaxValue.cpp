54097ab360e6215728c44efc91c8be50a777a083
Hyeri1ee <haerizian10@gmail.com>
2024-01-05 04:12:42 +0900

    baekjoon upload 1/5 ver.2

---
+++ algorithm1/baekjoon_bystep/2566_MaxValue.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int k[9][9];
+	int n, m;
+	int max = 0;	
+	for (int i = 0; i < 9; i++)
+		for (int j = 0; j < 9; j++) {
+			cin >> k[i][j];
+			if (max < k[i][j]) {
+				max = k[i][j];
+				n = i; m = j;
+			}
+		}
+	cout << max << endl;
+	cout << n+1 << " " << m+1;
+	return 0;
+}
